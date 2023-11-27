645fe4c19446cddd9fc8f3ec26e6e6c198624cb3
fabio <fabio8lauro@gmail.com>
2023-11-27 13:26:19 +0100

    tried to implement reduceLRonPlateau scheduler

--- lightning_model.py
+++ lightning_model.py
@@ -35,7 +35,17 @@ class CustomLightningModel(pl.LightningModule):
     def configure_optimizers(self):  # Method to configure optimizers
         # Using Stochastic Gradient Descent as the optimizer
         optimizers = torch.optim.Adam(self.parameters(), lr=1e-05, betas=(0.9, 0.999))
-        return optimizers
+        return {
+            "optimizer": optimizers,
+            "lr_scheduler": {
+                "scheduler": ReduceLROnPlateau(optimizers, mode=min, patience=2),
+                "interval": "epoch",
+                "monitor": "loss",
+                "frequency": 2
+                # If "monitor" references validation metrics, then "frequency" should be set to a
+                # multiple of "trainer.check_val_every_n_epoch".
+            },
+        }
 
     def loss_function(self, descriptors, labels):  # Method to compute loss
         loss = self.loss_fn(descriptors, labels)  # Compute Contrastive loss
