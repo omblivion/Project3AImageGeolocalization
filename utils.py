d44f6236b89977d6c10031175f6b1743095c64ef
fabio <fabio8lauro@gmail.com>
2023-12-01 15:34:10 +0100

    changed LR statement to 1e-4

--- utils.py
+++ utils.py
@@ -167,7 +167,7 @@ def print_model_configuration(model_instance):
     print_divider("Model Configuration")
     print(f"Model Architecture: {model_instance.model.__class__.__name__}")
     print(f"Pretrained: {'Yes' if model_instance.model.fc.in_features else 'No'}")  # Example check for pretrained
-    print(f"Optimizer: ADAM with lr = 1e-05, betas = (0.9, 0.999)")
+    print(f"Optimizer: ADAM with lr = 1e-04, betas = (0.9, 0.999)")
     print(f"Scheduler: Cosine Annealing with Tmax = 10 and eta_min=1e-6")
     print(f"Loss Function: {model_instance.loss_fn.__class__.__name__}")
 
