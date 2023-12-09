2f610645981ead7b776341d1d94b497cc6f29b78
fabio <fabio8lauro@gmail.com>
2023-12-09 17:39:41 +0100

    changed lr to 1e-3, still no scheduler

--- lightning_model.py
+++ lightning_model.py
@@ -35,7 +35,7 @@ class CustomLightningModel(pl.LightningModule):
     def configure_optimizers(self):  # Method to configure optimizers
         # Using Stochastic Gradient Descent as the optimizer
         # optimizers = torch.optim.Adam(self.parameters(), lr=1e-04, betas=(0.9, 0.999))
-        optimizers = torch.optim.AdamW(self.parameters(), lr=1e-05, betas=(0.9, 0.999), weight_decay=1e-2)
+        optimizers = torch.optim.AdamW(self.parameters(), lr=1e-03, betas=(0.9, 0.999), weight_decay=1e-2)
         # self.scheduler = ReduceLROnPlateau(optimizer, mode='min', patience=2, factor=0.5, verbose=True)
         # self.scheduler = CosineAnnealingLR(optimizers, T_max=10, eta_min=1e-6)
         # return {'optimizer': optimizers, 'lr_scheduler': self.scheduler, 'monitor': 'loss'}
