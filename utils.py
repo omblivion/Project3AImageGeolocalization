22ba937252b8733821a9e08d4b744ee2c781ee63
fabio <fabio8lauro@gmail.com>
2023-12-10 20:55:12 +0100

    trying to implement ReduceLrOnPlateau with adamW with lr=1e-4

--- utils.py
+++ utils.py
@@ -168,8 +168,8 @@ def print_model_configuration(model_instance):
     print(f"Model Architecture: {model_instance.model.__class__.__name__}")
     print(f"Pretrained: {'Yes' if model_instance.model.fc.in_features else 'No'}")  # Example check for pretrained
     print(f"Optimizer: AdamW with lr = 1e-4, betas = (0.9, 0.999), weight decay = 1e-2")
-    # print(f"Scheduler: Cosine Annealing with Tmax = 10 and eta_min=1e-6")
-    print(f"No scheduler used")
+    print(f"Scheduler: ReduceLRonPlateau, patience = 2, factor = 0.5")
+    # print(f"No scheduler used")
     print(f"Loss Function: {model_instance.loss_fn.__class__.__name__}")
 
 
